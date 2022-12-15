4ea2037441ea8977d5f8c5d1cfa325d74216f7d1
Alexey <redy22@mail.ru>
2022-12-15 17:49:24 +0400

    new

--- Program.cs
+++ Program.cs
@@ -19,42 +19,7 @@ Console.WriteLine();
 Console.WriteLine($"количество значений больше 0 = {sum}");
 
 
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
 
-    int[] numbers = new int [count];
-    int index = 0;
-
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
 
 
 void PrintArray(int[] array)
